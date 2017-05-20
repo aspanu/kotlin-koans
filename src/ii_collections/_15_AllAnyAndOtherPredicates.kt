e68e0be6dfc6c0c8507f42257d3ecd3fa2a64121
Adrian Spanu <adri@nspanu.ca>
2017-05-21 01:24:28 +0200

    Almost done the second collections folder as well.

--- src/ii_collections/_15_AllAnyAndOtherPredicates.kt
+++ src/ii_collections/_15_AllAnyAndOtherPredicates.kt
@@ -15,25 +15,25 @@ fun example2(list: List<Int>) {
 
 fun Customer.isFrom(city: City): Boolean {
     // Return true if the customer is from the given city
-    todoCollectionTask()
+    return this.city.equals(city)
 }
 
 fun Shop.checkAllCustomersAreFrom(city: City): Boolean {
     // Return true if all customers are from the given city
-    todoCollectionTask()
+    return this.customers.stream().allMatch { it.city.equals(city) }
 }
 
 fun Shop.hasCustomerFrom(city: City): Boolean {
     // Return true if there is at least one customer from the given city
-    todoCollectionTask()
+    return this.customers.stream().anyMatch { it.city.equals(city) }
 }
 
 fun Shop.countCustomersFrom(city: City): Int {
-    // Return the number of customers from the given city
-    todoCollectionTask()
+    return this.customers.stream().filter { it.city == city }.count().toInt()
 }
 
 fun Shop.findAnyCustomerFrom(city: City): Customer? {
     // Return a customer who lives in the given city, or null if there is none
-    todoCollectionTask()
+    val optionalCustomer = this.customers.stream().filter { it.city == city }.findFirst()
+    if (!optionalCustomer.isPresent) return null else return optionalCustomer.get()
 }
